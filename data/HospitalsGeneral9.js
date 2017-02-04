f5cb04b28cbab3b55a0ff547a62c963d6fbf5ae1
JinYuan <jinyuan90@hotmail.com>
2017-02-04 19:55:20 +0800

    initial commit

---
+++ data/HospitalsGeneral9.js
@@ -0,0 +1,17 @@
+var json_HospitalsGeneral9={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Type": "General", "Name": "Changi General Hospital", "Address": "2 Simei Street 3", "Postal": "529889", "Beds": "790" }, "geometry": { "type": "Point", "coordinates": [ 103.94935249899625, 1.340690066918847 ] } },
+{ "type": "Feature", "properties": { "Type": "General", "Name": "Gleneagles Hospital", "Address": "6A Napier Rd", "Postal": "258500", "Beds": "258" }, "geometry": { "type": "Point", "coordinates": [ 103.81942088940552, 1.307364735132654 ] } },
+{ "type": "Feature", "properties": { "Type": "General", "Name": "Khoo Teck Puat Hospital", "Address": "90 Yishun Central", "Postal": "768828", "Beds": "550" }, "geometry": { "type": "Point", "coordinates": [ 103.83823165095534, 1.423836178955193 ] } },
+{ "type": "Feature", "properties": { "Type": "General", "Name": "Mount Alvernia Hospital", "Address": "820 Thomson Rd", "Postal": "574623", "Beds": "335" }, "geometry": { "type": "Point", "coordinates": [ 103.83777444236131, 1.341498974793856 ] } },
+{ "type": "Feature", "properties": { "Type": "General", "Name": "National University Hospital", "Address": "5 Lower Kent Ridge Rd", "Postal": "119074", "Beds": "1250" }, "geometry": { "type": "Point", "coordinates": [ 103.7837257956494, 1.294835541917302 ] } },
+{ "type": "Feature", "properties": { "Type": "General", "Name": "Ng Teng Fong General Hospital & Jurong Community Hospital", "Address": "1 Jurong East Street 21", "Postal": "609606", "Beds": "1100" }, "geometry": { "type": "Point", "coordinates": [ 103.7466349639205, 1.332609436682444 ] } },
+{ "type": "Feature", "properties": { "Type": "General", "Name": "Parkway East Hospital (former East Shore Hospital)", "Address": "321 Joo Chiat Place", "Postal": "427990", "Beds": "106" }, "geometry": { "type": "Point", "coordinates": [ 103.90899142707622, 1.315068687586344 ] } },
+{ "type": "Feature", "properties": { "Type": "General", "Name": "Raffles Hospital", "Address": "585 North Bridge Rd", "Postal": "188770", "Beds": "380" }, "geometry": { "type": "Point", "coordinates": [ 103.85724371866615, 1.301143297687036 ] } },
+{ "type": "Feature", "properties": { "Type": "General", "Name": "Sengkang General Hospital", "Address": "1 Anchorvale Street", "Postal": "544835", "Beds": "1400" }, "geometry": { "type": "Point", "coordinates": [ 103.89233167714038, 1.395838430253203 ] } },
+{ "type": "Feature", "properties": { "Type": "General", "Name": "Singapore General Hospital", "Address": "Outram Rd", "Postal": "169608", "Beds": "1700" }, "geometry": { "type": "Point", "coordinates": [ 103.83677995098752, 1.281975008760215 ] } },
+{ "type": "Feature", "properties": { "Type": "General", "Name": "Tan Tock Seng Hospital", "Address": "11 Jalan Tan Tock Seng", "Postal": "308433", "Beds": "1500" }, "geometry": { "type": "Point", "coordinates": [ 103.84990055475498, 1.319499927709145 ] } }
+]
+}
